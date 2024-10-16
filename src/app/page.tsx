640d7b485edabc061b23b4b44b0aa270f84a552c
speardragon <rkdckdfyyd@naver.com>
2024-10-16 20:18:53 +0900

    fix(landing-page): 깃허브 로그인 버튼 위치 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,23 +13,24 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <header className="flex w-full justify-end p-2">
-        <Button onClick={handleKakaoLogin} className="flex gap-2">
-          <Image
-            alt="github_white_logo"
-            src={GITHUB_WHITE_LOGO}
-            width={20}
-            height={20}
-          />
-          <div className="font-semibold">깃허브로 로그인하기</div>
-        </Button>
-      </header>
+      {/* <header className="flex w-full justify-end p-2">
+        
+      </header> */}
       <main className="flex flex-1 justify-center items-center">
         <div className="flex flex-col items-center gap-2">
           <Image alt="github_white_logo" src={GITFOLIO_LOGO} width={300} />
-          <div className="text-blue-600">
+          <div className="text-blue-600 mb-6">
             한 큐에 만드는 나만의 이력서, <strong>깃트폴리오</strong>
           </div>
+          <Button onClick={handleKakaoLogin} className="flex gap-2">
+            <Image
+              alt="github_white_logo"
+              src={GITHUB_WHITE_LOGO}
+              width={20}
+              height={20}
+            />
+            <div className="font-semibold">깃허브로 로그인하기</div>
+          </Button>
           <Image alt="github_white_logo" src={LANDING_IMAGE} width={1000} />
         </div>
       </main>
